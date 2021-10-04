a7484cd20971551e91f766b5c4afbcf7e8579a4a
Alissa Woon 2 <alissawoon@gmail.com>
2021-10-04 20:20:04 +0800

    819

--- Geolocation/js/haversine_main.js
+++ Geolocation/js/haversine_main.js
@@ -1,4 +1,4 @@
-function calculateDistance(lat, long){
+function calculateDistance(lat, lon){
     let depulze = haversine(lat, lon,2.922561,101.650965)
     let pyramid = haversine(lat, lon,3.073065,101.607787)      
     let klcc = haversine(lat, lon,3.158761,101.714524)      
